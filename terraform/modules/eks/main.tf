a2305ff370a8522057650a00dc968907aa0b32ae
lucasKuhnLK <lucas.kuhn@authsoftsolutions.com>
2026-05-08 13:44:22 -0300

    feat(eks): aumenta node group tm-eks-ng-01 para 5 nodes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -29,8 +29,8 @@ resource "aws_eks_node_group" "nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 4
-    max_size     = 4
+    desired_size = 5
+    max_size     = 5
     min_size     = 1
   }
 
